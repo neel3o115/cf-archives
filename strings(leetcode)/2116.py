339be348c8ca31c7609f6436a29c6a222a1103fc
neel3o115 <neelarm.code@gmail.com>
2025-01-12 23:33:16 +0530

    11:33

--- strings(leetcode)/2116.py
+++ strings(leetcode)/2116.py
@@ -1,19 +1,31 @@
-s = "(()())"
-locked = "111111"
-
-nee = True
-if len(s) % 2 != 0:
-    nee = False
-
-else:
+def canBeValid(s, locked):
+    if len(s) % 2 != 0:
+        return False
     
-    for i in range(len(s)):
-        if i % 2 == 0 and s[i] == ")" and locked[i] == "1":
-            nee = False
-            break
+    n = len(s)
+    o = 0
+    c = 0
 
-        elif i % 2 != 0 and s[i] == "(" and locked[i] == "1":
-            nee = False
-            break
+    for i in range(n):
+        if locked[i] == "0" or s[i] == "(":
+            o += 1
+        else:
+            o -= 1
+        
+        if o < 0:
+            return False
+    
+    for i in range(n - 1, -1, -1):
+        if locked[i] == "0" or s[i] == ")":
+            c += 1
+        else:
+            c -= 1
+        
+        if c < 0:
+            return False
 
-print(nee)
+    return True
+
+s = "(()())()"
+locked = "11111111"
+print(canBeValid(s, locked))
