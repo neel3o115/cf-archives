2f9495889294ee056fc601b0663f3f0c3421390d
neel3o115 <neelarm.code@gmail.com>
2025-01-19 21:40:57 +0530

    9:40

--- B_Farmer_John_s_Card_Game.py
+++ B_Farmer_John_s_Card_Game.py
@@ -1,8 +1,39 @@
+def fun(mat, k):
+    nee = []
+    for row in mat:
+        new = [x % k for x in row]
+        nee.append(new)
+    return nee
+
 t = int(input())
 
 for _ in range(t):
     n, m = map(int, input().split()) 
     
+    mm = []
     for i in range(n):
-        a = int(input())
+        a = list(map(int, input().split()))
+        mm.append(a)
+
+    if n == 1:
+        print(1)
     
+    else:
+        jee = fun(mm, n)
+
+        kee = dict()
+        count = 0
+        for i in range(n):
+            if all (x == jee[i][0] for x in jee[i]):
+                count += 1
+                kee[jee[i][0]] = i + 1
+        
+        if count == len(jee):
+            keeee = {key: kee[key] for key in sorted(kee)}
+            ooo = []
+            for k,v in keeee.items():
+                ooo.append(v)
+            print(*ooo)
+
+        else:
+            print(-1)
