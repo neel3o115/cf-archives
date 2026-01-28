3fb35f7c3d44d745af6f43eaea49905e9dbf2e9f
neel3o115 <neelarm.code@gmail.com>
2026-01-28 21:23:00 +0530

    solve problem

---
+++ neel/M_Ostap_and_Grasshopper.py
@@ -0,0 +1,26 @@
+n , k = map(int , input().split())
+
+li = list(input())
+l = 0
+r = n-1
+
+g = -1
+t = -1
+
+for i in range(n):
+    if li[i] == "G":
+        g = i
+        break
+    elif li[i] == "T":
+        t = i
+        break
+
+ans = "NO"
+
+for i in range(max(g,t)+k,n,k):
+    if li[i] in ["G","T"]:
+        ans = "YES"
+    elif li[i] == "#":
+        break
+
+print(ans)
