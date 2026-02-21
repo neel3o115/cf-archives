993bd87cf9ee71085c41e93cf43029e7c78d63c1
neel3o115 <neelarm.code@gmail.com>
2026-02-21 22:04:00 +0530

    daily problems

---
+++ neel/CSES_Sum_of_Divisors.cpp
@@ -0,0 +1,91 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+// ---------- debug utilities ----------
+#ifndef ONLINE_JUDGE
+    #include <debug.cpp>
+    auto start = chrono::high_resolution_clock::now();
+#else 
+    #define debug(...)
+    #define debugArr(...)
+#endif
+ 
+ 
+// ---------- shortcut macros ----------
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define len(x) (int)(x).size()
+#define pb push_back
+#define print1(zzz) for(auto zzzz: zzz) cout << zzzz << ' ';cout<<endl;
+#define print2(zzz) for (auto &zzzzz : zzz) {print1(zzzzz)}
+#define space cout << endl;
+#define sum(v) (accumulate((v).begin(), (v).end(), 0LL))
+#define lb(v, x) (lower_bound((v).begin(), (v).end(), (x)) - (v).begin())  
+#define ub(v, x) (upper_bound((v).begin(), (v).end(), (x)) - (v).begin())  
+#define execute ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+ 
+// ----------- constants -----------
+#define PI 3.14159265358979323846
+#define INF 1e18
+#define MOD 1000000007
+#define MOD2 998244353
+ 
+// ---------- type aliases ----------
+#define ll long long
+#define int long long
+#define vi vector<int> 
+#define vc vector<char> 
+#define vs vector<string> 
+#define vvc vector<vector<char>> 
+#define vvi vector<vector<int>> 
+#define pii pair<int, int> 
+ 
+// ---------- input macros ----------
+#define msb(x) (x == 0 ? -1 : 1ll<<(63 - __builtin_clzll(x)))      
+#define lsb(x) (x == 0 ? -1 : 1ll<<(__builtin_ctzll(x)))   
+#define pc(x) (__builtin_popcountll(x))
+ll gcd(ll a, ll b) {return b ? gcd(b, a % b) : a;}
+ll lcm(ll a, ll b) {return a * b / gcd(a, b);}
+ll mod_mul(ll a,ll b) {return ((a % MOD) * (b % MOD)) % MOD;}
+ll mod_add(ll a,ll b) {return ((a % MOD) + (b % MOD)) % MOD;}
+ll mod_sub(ll a,ll b) {return ((a % MOD) - (b % MOD) + MOD) % MOD;}
+ll mod_pow(ll a,ll b, ll m = MOD) {ll res = 1;a %= m;while (b) {if (b & 1ll) res = res * a % m;a = mod_mul(a, a);b >>= 1ll;}return res;}
+ll mod_inv(ll a, ll m = MOD) {return mod_pow(a, m - 2, m);}  // fermat's little theorem
+ll mod_div(ll a, ll b) {return mod_mul(a, mod_inv(b));}
+
+
+int fun(int x, int n) {
+    int y = n/x;
+    return mod_mul(mod_mul(y-x, (x+1+y)), mod_inv(2));
+}
+
+void solve() {
+    int n; cin>>n; 
+    int ans = 0;
+ 
+    for (int i=1; i*i<=n; i++) {
+        // cout << i << " " << ((n/i)-(i-1))*i << " " << fun(i, n) << endl;
+        ans = mod_add(((n/i)-(i-1))*i, ans);
+        ans = mod_add(fun(i, n), ans);
+    }
+ 
+    cout << ans <<endl;
+}
+ 
+int32_t main(){
+    // cout << fixed << setprecision(10);
+ 
+    execute
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+ 
+    #ifndef ONLINE_JUDGE
+        chrono::duration<double> elapsed = chrono::high_resolution_clock::now() - start;
+        cerr << fixed << setprecision(6) << "\nTime: " << elapsed.count() << "s\n"; 
+    #endif
+    return 0;
+}
