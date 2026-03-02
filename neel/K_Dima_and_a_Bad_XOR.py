6941d5ccaa710b2d2f8475dc43087956db8205a8
neel3o115 <neelarm.code@gmail.com>
2026-03-02 15:40:00 +0530

    cp practice

---
+++ neel/K_Dima_and_a_Bad_XOR.py
@@ -0,0 +1,26 @@
+PI = 3.14159265358979323846
+E = 2.718281828459045
+INF = 1000000000000000000
+MOD1 = 1000000007
+MOD2 = 998244353
+
+def solve():
+    n,m = map(int,input().split())
+    a = []
+
+    for i in range(n):
+        row = list(map(int,input().split()))
+        a.append(row)
+    
+    xor = a[0][0]
+
+    for i in a:
+        print(*i)
+
+def main():
+    t = 1
+    # t = int(input()) 
+    for _ in range(t):
+        solve()
+
+main()
