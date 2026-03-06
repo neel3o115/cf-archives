e05aab763ace029c16450c6d8f5eaedbe633ce2f
neel3o115 <neelarm.code@gmail.com>
2026-03-06 10:16:00 +0530

    practice session

---
+++ neel/C_Countless_Me.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------- debug utilities ----------
+#ifndef Local
+    #define debug(...)
+    #define debugArr(...)
+#else
+    #include <debug.cpp>
+    auto start = chrono::high_resolution_clock::now();
+#endif
+
+
+// ---------- shortcut macros ----------
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define len(x) (int)(x).size()
+#define pb push_back
+#define print(zzz) cout << zzz << ' '<<endl;
+#define print1(zzz) for(auto zzzz: zzz) cout << zzzz << ' ';cout<<endl;
+#define print2(zzz) for (auto &zzzzz : zzz) {print1(zzzzz)}
+#define space cout << endl;
+#define sum(v) (accumulate((v).begin(), (v).end(), 0LL))
+#define lb(v, x) (lower_bound((v).begin(), (v).end(), (x)) - (v).begin())  
+#define ub(v, x) (upper_bound((v).begin(), (v).end(), (x)) - (v).begin())  
+#define execute ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+// ----------- constants -----------
+#define PI 3.14159265358979323846
+#define INF 1e18
+#define MOD 1000000007
+#define MOD2 998244353
+
+// ---------- type aliases ----------
+#define ll long long
+#define int long long
+#define vi vector<int> 
+#define vc vector<char> 
+#define vs vector<string> 
+#define vvc vector<vector<char>> 
+#define vvi vector<vector<int>> 
+#define pii pair<int, int> 
+
+// ---------- input macros ----------
+#define msb(x) (x == 0 ? -1 : 1ll<<(63 - __builtin_clzll(x)))      
+#define lsb(x) (x == 0 ? -1 : 1ll<<(__builtin_ctzll(x)))   
+#define pc(x) (__builtin_popcountll(x))
+ll gcd(ll a, ll b) {return b ? gcd(b, a % b) : a;}
+ll lcm(ll a, ll b) {return a * b / gcd(a, b);}
+
+int ans = 0;
+
+void nee(int x, int n) {
+    if (x <= 0) return;
+    int p = 0;
+
+    while (x-pow(2,p)*n > (pow(2,p)-1)*n) p++;
+
+    ans += pow(2,p);
+    int nx = x-(pow(2,p)*n);
+
+    if (nx >= 0) {
+        nee(nx, n);
+    } else {
+        int nn = n;
+        while (x-(pow(2,p)*(nn)) < 0) nn--;
+        nee(x-(pow(2,p)*nn), n);
+    }
+}
+
+void solve() {
+    int n; cin>>n;
+    vi a(n);
+
+    for (int i=0; i<n; i++) cin>>a[i];
+    sort(all(a));
+    int x = sum(a);
+
+    nee(x, n);
+    print(ans);
+}
+
+int32_t main(){
+    // cout << fixed << setprecision(10);
+    #ifdef Local
+        // freopen("input.txt", "r+", stdin);
+        // freopen("output.txt", "w+", stdout);
+    #endif
+
+    execute
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    #ifdef Local
+        chrono::duration<double> elapsed = chrono::high_resolution_clock::now() - start;
+        cerr << fixed << setprecision(6) << "\nTime: " << elapsed.count() << "s\n"; 
+    #endif
+    return 0;
+}
