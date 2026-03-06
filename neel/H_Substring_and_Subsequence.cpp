2114bd47a595e07c241ff50e0a7d4a30375b429d
neel3o115 <neelarm.code@gmail.com>
2026-03-06 11:49:00 +0530

    daily problems

---
+++ neel/H_Substring_and_Subsequence.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    int t;cin>>t;
+    while(t--){
+        string a,b;cin>>a>>b;
+        int i=0,j=0;
+        int count1=0,maxm=0;
+        while(i<a.size() and j<b.size()){
+            if(a[i]!=b[j]){
+                j++;
+            }else{
+                count1++;
+                for(int k=1;k<a.size();k++){
+                    if(a[k])
+                }
+            }
+        }
+    }
+}
