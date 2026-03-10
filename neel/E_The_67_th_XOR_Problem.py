dca5bfbe8dfe1a10ce49f55c6fa8ef8c19e659a3
neel3o115 <neelarm.code@gmail.com>
2026-03-10 15:59:00 +0530

    cp practice

---
+++ neel/E_The_67_th_XOR_Problem.py
@@ -0,0 +1,27 @@
+PI = 3.14159265358979323846
+E = 2.718281828459045
+INF = 1000000000000000000
+MOD1 = 1000000007
+MOD2 = 998244353
+
+def solve():
+    n = int(input())
+    a = list(map(int, input().split())) 
+
+    res = 0
+
+    for i in range(n):
+        for j in range(n):
+            res = max(res,a[i]^a[j])
+
+    print(res)
+
+def main():
+    t = 1
+    t = int(input()) 
+    for _ in range(t):
+        solve()
+
+main()
+
+# print(int('1111111100',2))
