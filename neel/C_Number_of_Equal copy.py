6642420df319a80dbf4efdddd0f648b1c008de92
neel3o115 <neelarm.code@gmail.com>
2026-03-19 13:25:00 +0530

    add solution

---
+++ neel/C_Number_of_Equal copy.py
@@ -0,0 +1,22 @@
+n, m = map(int, input().split())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+cn  = 0
+
+# for i in range(n):
+#     for j in range(m):
+#         if a[i] == b[j]:
+#             cn += 1
+
+i = 0
+j = 0
+
+# while i < n and j < m:
+#     x, y = 1, 1
+
+
+
+
+
+print(cn)
+print(9//10)
