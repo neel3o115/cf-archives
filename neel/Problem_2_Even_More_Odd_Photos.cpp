400ed0763bd41dba75865e951c61815831035721
neel3o115 <neelarm.code@gmail.com>
2026-03-25 10:29:00 +0530

    practice session

---
+++ neel/Problem_2_Even_More_Odd_Photos.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------- debug utilities ----------
+#ifndef Local
+    #define debug(...)
+    #define debugArr(...)
+#else
+    #include <debug.cpp>
+    auto start = chrono::high_resolution_clock::now();
+#endif
+
+
+// ---------- shortcut macros ----------
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define len(x) (int)(x).size()
+#define pb push_back
+#define print(zzz) cout << zzz << ' '<<endl;
+#define print1(zzz) for(auto zzzz: zzz) cout << zzzz << ' ';cout<<endl;
+#define print2(zzz) for (auto &zzzzz : zzz) {print1(zzzzz)}
+#define space cout << endl;
+#define sum(v) (accumulate((v).begin(), (v).end(), 0LL))
+#define lb(v, x) (lower_bound((v).begin(), (v).end(), (x)) - (v).begin())  
+#define ub(v, x) (upper_bound((v).begin(), (v).end(), (x)) - (v).begin())  
+#define execute ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+// ----------- constants -----------
+#define PI 3.14159265358979323846
+#define INF 1e18
+#define MOD 1000000007
+#define MOD2 998244353
+
+// ---------- type aliases ----------
+#define ll long long
+#define int long long
+#define vi vector<int> 
+#define vc vector<char> 
+#define vs vector<string> 
+#define vvc vector<vector<char>> 
+#define vvi vector<vector<int>> 
+#define pii pair<int, int> 
+
+// ---------- input macros ----------
+#define msb(x) (x == 0 ? -1 : 1ll<<(63 - __builtin_clzll(x)))      
+#define lsb(x) (x == 0 ? -1 : 1ll<<(__builtin_ctzll(x)))   
+#define pc(x) (__builtin_popcountll(x))
+ll gcd(ll a, ll b) {return b ? gcd(b, a % b) : a;}
+ll lcm(ll a, ll b) {return a * b / gcd(a, b);}
+
+void solve() {
+    int n; cin>>n;
+
+    int eve = 0;
+    int odd = 0;
+
+    for (int i=0; i<n; i++) {
+        int x; cin>>x;
+
+        if (x%2 == 0) eve++;
+        else odd++;
+    }
+
+    int res = 0; 
+    int crr = 0;
+
+    while (eve>0 or odd>0) {
+        if (crr == 0) {
+            if (eve > 0) {
+                eve--;
+                res++;
+                crr=1;
+            } else if (odd >= 2) {
+                odd = odd-2;
+                res++;
+                crr=1;
+            } else {
+                res--;
+                break;
+            }
+        } else {
+            if (odd > 0) {
+                odd--;
+                res++;
+                crr=0;
+            } else {
+                break;
+            }
+        }
+    }
+
+    cout << res;
+}
+
+int32_t main(){
+    // cout << fixed << setprecision(10);
+    #ifdef Local
+        // freopen("input.txt", "r+", stdin);
+        // freopen("output.txt", "w+", stdout);
+    #endif
+
+    execute
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    #ifdef Local
+        chrono::duration<double> elapsed = chrono::high_resolution_clock::now() - start;
+        cerr << fixed << setprecision(6) << "\nTime: " << elapsed.count() << "s\n"; 
+    #endif
+    return 0;
+}
