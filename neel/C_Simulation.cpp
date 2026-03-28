253bc49fc6a6e556e174ac1d8cfc1d41264aa850
neel3o115 <neelarm.code@gmail.com>
2026-03-28 14:22:00 +0530

    add solution

---
+++ neel/C_Simulation.cpp
@@ -0,0 +1,102 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------- debug utilities ----------
+#ifndef Local
+    #define debug(...)
+    #define debugArr(...)
+#else
+    #include <debug.cpp>
+    auto start = chrono::high_resolution_clock::now();
+#endif
+
+
+// ---------- shortcut macros ----------
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define len(x) (int)(x).size()
+#define pb push_back
+#define print(zzz) cout << zzz << ' '<<endl;
+#define print1(zzz) for(auto zzzz: zzz) cout << zzzz << ' ';cout<<endl;
+#define print2(zzz) for (auto &zzzzz : zzz) {print1(zzzzz)}
+#define space cout << endl;
+#define sum(v) (accumulate((v).begin(), (v).end(), 0LL))
+#define lb(v, x) (lower_bound((v).begin(), (v).end(), (x)) - (v).begin())  
+#define ub(v, x) (upper_bound((v).begin(), (v).end(), (x)) - (v).begin())  
+#define execute ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+// ----------- constants -----------
+#define PI 3.14159265358979323846
+#define INF 1e18
+#define MOD 1000000007
+#define MOD2 998244353
+
+// ---------- type aliases ----------
+#define ll long long
+#define int long long
+#define vi vector<int> 
+#define vc vector<char> 
+#define vs vector<string> 
+#define vvc vector<vector<char>> 
+#define vvi vector<vector<int>> 
+#define pii pair<int, int> 
+
+// ---------- input macros ----------
+#define msb(x) (x == 0 ? -1 : 1ll<<(63 - __builtin_clzll(x)))      
+#define lsb(x) (x == 0 ? -1 : 1ll<<(__builtin_ctzll(x)))   
+#define pc(x) (__builtin_popcountll(x))
+ll gcd(ll a, ll b) {return b ? gcd(b, a % b) : a;}
+ll lcm(ll a, ll b) {return a * b / gcd(a, b);}
+
+
+
+void solve() {
+    vector<int> initial;
+    int a = 1, b = 1;
+    int cur = 1;
+    int i = 0;
+    // vector<int> cur = initial;
+
+    // for (int i = 0; i < 10; ++i) {
+    //     vector<int> temp;
+    //     for (int j = 0; j < cur.size() - 1; ++j) {
+    //         temp.pb(cur[j]);
+    //         temp.pb(cur[j] + cur[j+1]);
+    //     }
+    //     cur = temp;
+    //     for (auto &e : temp) { cout << e << ' ';}
+    //     cout << endl;
+    //     cout << *max_element(all(temp));
+    //     cout << endl << endl;
+    // }
+
+    while (cur <= 1000000000) {
+        i++;
+        cur = a + b;
+        cout << cur << ' ';
+        swap(a,b);
+        a = cur;
+    }
+    cout << i << endl;
+}
+
+int32_t main(){
+    // cout << fixed << setprecision(10);
+    #ifdef Local
+        // freopen("input.txt", "r+", stdin);
+        // freopen("output.txt", "w+", stdout);
+    #endif
+
+    execute
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    #ifdef Local
+        chrono::duration<double> elapsed = chrono::high_resolution_clock::now() - start;
+        cerr << fixed << setprecision(6) << "\nTime: " << elapsed.count() << "s\n"; 
+    #endif
+    return 0;
+}
