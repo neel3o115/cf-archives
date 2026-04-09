0a8bb73f5cd46a7f5041f678bb68b5ddb9c4720f
neel3o115 <neelarm.code@gmail.com>
2026-04-09 11:52:00 +0530

    daily problems

---
+++ neel/Problem_1_Don_t_Be_Last.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// ---------- debug utilities ----------
+#ifndef Local
+    #define debug(...)
+    #define debugArr(...)
+#else
+    #include <debug.cpp>
+    auto start = chrono::high_resolution_clock::now();
+#endif
+
+
+// ---------- shortcut macros ----------
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define len(x) (int)(x).size()
+#define pb push_back
+#define print(zzz) cout << zzz << ' '<<endl;
+#define print1(zzz) for(auto zzzz: zzz) cout << zzzz << ' ';cout<<endl;
+#define print2(zzz) for (auto &zzzzz : zzz) {print1(zzzzz)}
+#define space cout << endl;
+#define sum(v) (accumulate((v).begin(), (v).end(), 0LL))
+#define lb(v, x) (lower_bound((v).begin(), (v).end(), (x)) - (v).begin())  
+#define ub(v, x) (upper_bound((v).begin(), (v).end(), (x)) - (v).begin())  
+#define execute ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+// ----------- constants -----------
+#define PI 3.14159265358979323846
+#define INF 1e18
+#define MOD 1000000007
+#define MOD2 998244353
+
+// ---------- type aliases ----------
+#define ll long long
+#define int long long
+#define vi vector<int> 
+#define vc vector<char> 
+#define vs vector<string> 
+#define vvc vector<vector<char>> 
+#define vvi vector<vector<int>> 
+#define pii pair<int, int> 
+
+// ---------- input macros ----------
+#define msb(x) (x == 0 ? -1 : 1ll<<(63 - __builtin_clzll(x)))      
+#define lsb(x) (x == 0 ? -1 : 1ll<<(__builtin_ctzll(x)))   
+#define pc(x) (__builtin_popcountll(x))
+ll gcd(ll a, ll b) {return b ? gcd(b, a % b) : a;}
+ll lcm(ll a, ll b) {return a * b / gcd(a, b);}
+
+void solve() {
+    int n; cin>>n;
+    map<string,int> milk;
+
+    milk["Annabelle"] = 0;
+    milk["Bessie"] = 0;
+    milk["Daisy"] = 0;
+    milk["Elsie"] = 0;
+    milk["Gertie"] = 0;
+    milk["Henrietta"] = 0;
+    milk["Maggie"] = 0;
+
+    for (int i=0; i<n; i++) {
+        string s; 
+        int x;
+
+        cin>>s>>x;
+        milk[s] += x; 
+    }
+
+    set<int> nee;
+    vi jee;
+
+    for (auto it: milk) nee.insert(it.second);
+    for (int it: nee) jee.pb(it);
+
+    if (len(jee) == 1) {
+        cout << "Tie";
+    } else {
+        int x = jee[1];
+        set<string> res;
+
+        for (auto it: milk) {
+            if (it.second == x) res.insert(it.first);
+        }
+
+        if (len(res) > 1) {
+            cout << "Tie";
+        } else { 
+            for (auto it: res) {
+                cout << it;
+                return;
+            }
+        }
+    }
+}
+
+int32_t main(){
+    // cout << fixed << setprecision(10);
+        freopen("notlast.in", "r+", stdin);
+        freopen("notlast.out", "w+", stdout);
+
+    execute
+    int t = 1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    
+    #ifdef Local
+        chrono::duration<double> elapsed = chrono::high_resolution_clock::now() - start;
+        cerr << fixed << setprecision(6) << "\nTime: " << elapsed.count() << "s\n"; 
+    #endif
+    return 0;
+}
